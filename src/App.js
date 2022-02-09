a217af90acf468c83dec4f62f5b43b810d309d89
Enes Şahin <enes.cyclones@gmail.com>
2022-02-09 18:23:47 +0300

    some gui update

--- src/App.js
+++ src/App.js
@@ -19,8 +19,8 @@ function App() {
     return (
         <div className="min-h-screen bg-white opacity-75 bg-gray-100">
             <div className="container m-auto w-full h-full pt-24 flex flex-col items-center justify-center">
-                <div className={"lg:w-1/2 sm:w-5/6 shadow-2xl bg-amber-50 m-10"}><NoteBody/></div>
-                <div className={"w-5/6 m-10"}><NoteList/></div>
+                <div className={"lg:w-1/2 sm:w-full shadow-2xl bg-amber-50  m-3 px-2 py-2"}><NoteBody/></div>
+                <div className={"lg:w-5/6 md:w-5/6 sm:w-full m-3 px-2 py-2"}><NoteList/></div>
             </div>
         </div>
     );
